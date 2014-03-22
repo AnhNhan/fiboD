3db10063bcd78a4d2cc2467bfbb44f0b303cca46
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-03-22 11:31:08 +0100

    Adding a recursive fibonacci implementation with memoization per a dynamic array.

--- source/recursive.d
+++ source/recursive.d
@@ -13,6 +13,10 @@ num fiboRec(in num nth)
     return fiboRec(nth - 1) + fiboRec(nth - 2);
 }
 
+/**
+ * Recursive Fibonacci with an associative array for memoization.
+ */
+
 alias Memo = num[num];
 enum MemoInitVal = num.max;
 
@@ -45,6 +49,48 @@ num fiboRecMemo(in num nth, ref Memo memo)
     if (memo[nth] == MemoInitVal)
     {
         memo[nth] = fiboRecMemo(nth - 1, memo) + fiboRecMemo(nth - 2, memo);
+    }
+
+    return memo[nth];
+}
+
+/**
+ * Recursive Fibonacci with a dynamic array for memoization.
+ */
+
+alias DynamicMemo = num[];
+
+// Template param nth_t is only for initialization.
+num fiboRecDynamicMemo(in num nth)
+{
+    if (nth == 0)
+        return 0;
+    if (nth == 1)
+        return 1;
+
+    DynamicMemo memo;
+    memo.length = nth + 1;
+
+    for (int ii; ii <= nth; ++ii)
+    {
+        memo[ii] = MemoInitVal;
+    }
+
+    memo[nth] = fiboRecDynamicMemo(nth - 1, memo) + fiboRecDynamicMemo(nth - 2, memo);
+
+    return memo[nth];
+}
+
+num fiboRecDynamicMemo(in num nth, ref DynamicMemo memo)
+{
+    if (nth == 0)
+        return 0;
+    if (nth == 1)
+        return 1;
+
+    if (memo[nth] == MemoInitVal)
+    {
+        memo[nth] = fiboRecDynamicMemo(nth - 1, memo) + fiboRecDynamicMemo(nth - 2, memo);
     }
 
     return memo[nth];
